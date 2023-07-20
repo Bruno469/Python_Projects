78044b34c65ac959ae518062e4aa63a290f557bd
Bruno469 <122754415+Bruno469@users.noreply.github.com>
2023-07-20 15:49:33 -0300

    Update and rename main.py to HoloLive.py
    Bugs fix and rename the file

--- HoloLive.py
+++ HoloLive.py
@@ -1,21 +1,21 @@
-#DiscordBot
-
 import discord
 
-client = discord.Client()
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print('Estou online como {0.user}'.format(client))
+    print(f'We have logged in as {client.user}')
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
-    
-    if message.content.startwith('!Olá'):
-        await message.channel.send('Olá ')
 
+    if message.content.startswith('!Olá'):
+        await message.channel.send(f'Olá! {message.author} :smiling_face_with_3_hearts:')
 client.run('MTA2NDYzOTQ4ODI4MzkxODM3Ng.GALGpQ.3l1ztVOcvtL_BfrAy6H7MyalmSOWbr3v5iCgB8')
 
 #MTA2NDYzOTQ4ODI4MzkxODM3Ng.GALGpQ.3l1ztVOcvtL_BfrAy6H7MyalmSOWbr3v5iCgB8

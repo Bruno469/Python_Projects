fd101c552ff81dca8eb9d41902fb5f028d200a92
Bruno469 <122754415+Bruno469@users.noreply.github.com>
2023-10-25 16:52:39 -0300

    Update ServidorUDP.py

--- ServidorUDP.py
+++ ServidorUDP.py
@@ -1,24 +1,10 @@
 import socket
 
-# Endereço e porta em que o servidor vai escutar
-host = '127.0.0.1'
-port = 12345
+host = '192.168.56.1'
+port = 176
 
-# Cria um objeto de socket UDP
-udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-# Vincula o socket ao endereço e porta especificados
-udp_socket.bind((host, port))
-
-print(f"Servidor UDP aguardando conexões em {host}:{port}")
-
-while True:
-    # Recebe dados e endereço do cliente
-    data, addr = udp_socket.recvfrom(1024)
-    
-    print(f"Recebido de {addr}: {data.decode('utf-8')}")
-
-    # Aqui você pode adicionar lógica para processar os dados recebidos
-
-# Feche o socket quando terminar
-udp_socket.close()
+def comando(mensagem):
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp_socket.bind((host, port))
+    print(f"Servidor UDP aguardando conexões em {host}:{port}")
+    udp_socket.sendto(mensagem.encode('utf-8'), (host, port))
